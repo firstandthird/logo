ac72851602251b09589efa6f6fefdaa75a0ec951
Greg Allen <me@jga.me>
2015-02-04 16:18:01 -0800

    be able to store colors in localStorage, share url with colors set

--- src/scripts/logo.js
+++ src/scripts/logo.js
@@ -1,5 +1,6 @@
 angular.module('logo', [
-  'yamldata'
+  'yamldata',
+  'ngStorage'
 ])
 .directive('css', function() {
   return {
@@ -16,14 +17,30 @@ angular.module('logo', [
 })
 .directive('bgColor', function() {
 })
-.controller('ColorizerController', function($scope, COLORS) {
+.controller('ColorizerController', function($scope, COLORS, $localStorage, $location) {
   $scope.mark = 'both';
-  $scope.colorOptions = COLORS;
+  var storedColors = $localStorage.colors || [];
+
+  $scope.colorOptions = COLORS.concat(storedColors);
 
   $scope.setColors = function(colors) {
     $scope.primaryColor = colors[0];
     $scope.secondaryColor = colors[1];
     $scope.backgroundColor = colors[2];
+    $location.search('colors', colors.join('_'));
   };
-  $scope.setColors($scope.colorOptions[0]);
+  var urlParams = $location.search();
+  var urlColor = urlParams.colors;
+  if (urlColor) {
+    $scope.setColors(urlColor.split('_'));
+  } else {
+    $scope.setColors($scope.colorOptions[0]);
+  }
+
+  $scope.save = function() {
+    var color = [$scope.primaryColor, $scope.secondaryColor, $scope.backgroundColor];
+    storedColors.push(color);
+    $scope.colorOptions.push(color);
+    $localStorage.colors = storedColors;
+  };
 });
